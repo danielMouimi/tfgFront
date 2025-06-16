8e984bbb9d0274d7971de04c0848a7fc41921473
Daniel Mouimi <mouimidaniel@gmail.com>
2025-06-16 13:20:57 +0200

    deploy

--- src/Acciones.jsx
+++ src/Acciones.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
 
 export function Acciones() {
     const navigate = useNavigate();
